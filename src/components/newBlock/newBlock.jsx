4f0ef941e1e5292689f1ff5687131a21763991b1
NanGua-QWQ <biancheng1688@163.com>
2026-02-26 21:42:44 +0800

    牛逼

--- src/components/newBlock/newBlock.jsx
+++ src/components/newBlock/newBlock.jsx
@@ -16,28 +16,45 @@ function moveUp(array, index, pos = 1) {
 }
 
 const NewBlock = props => {
+    // props.onSave(blockData)
+    // props.initialBlock (optional) - existing block to edit
     const [nowSvgBlock, setSvgBlock] = useState({});
     const [blockType, setBlocktype] = useState(BlockType.STACK);
     const [blockPart, setBlockPart] = useState([]);
+    const [colors, setColors] = useState({
+        primary: returnValue("comments").color[0],
+        secondary: returnValue("comments").color[1],
+        tertiary: returnValue("comments").color[2],
+    });
 
     const svgHTML = renderBlockToHTML(nowSvgBlock);
 
     const updateSVG = () => {
         const newBlock = {
             type: blockType,
-            colors: {
-                primary: returnValue("comments").color[0],
-                secondary: returnValue("comments").color[1],
-                tertiary: returnValue("comments").color[2],
-            },
+            colors,
             parts: blockPart
         };
         setSvgBlock(newBlock);
     };
 
+    // respond to type/parts changes
     useEffect(() => {
         updateSVG();
     }, [blockType, blockPart]);
+
+    // when editing an existing block, populate fields
+    useEffect(() => {
+        if (props.initialBlock) {
+            setBlocktype(props.initialBlock.type || BlockType.STACK);
+            setBlockPart(props.initialBlock.parts || []);
+            if (props.initialBlock.colors) {
+                setColors(props.initialBlock.colors);
+            }
+            // immediately update svg block to match colors
+            setSvgBlock(props.initialBlock);
+        }
+    }, [props.initialBlock]);
 
     return (
         <div>
@@ -89,6 +106,15 @@ const NewBlock = props => {
                                     )
                                 }}>
                                     Add Input
+                                </button>
+                                <button onClick={() => {
+                                    // 保存当前积木并关闭弹窗
+                                    if (props.onSave) {
+                                        props.onSave(nowSvgBlock);
+                                    }
+                                    props.close();
+                                }}>
+                                    Save Block
                                 </button>
                             </div>
                         )}
